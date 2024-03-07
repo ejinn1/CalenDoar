1a18cfa432ed8920f9f24a5fe4e83fed436d573e
ejinn1 <ejinn1@naver.com>
2024-03-07 19:12:41 +0900

    feat: useCalendarState 적용, now를 viewDate로 수정

--- src/app/_component/calendar/index.tsx
+++ src/app/_component/calendar/index.tsx
@@ -1,4 +1,8 @@
-import { getDaysInMonth } from "@/utils/date";
+"use client";
+
+import useCalendarState from "@/store/calendarDay";
+import Image from "next/image";
+import { useEffect } from "react";
 import tw from "tailwind-styled-components";
 
 const WeekContainer = tw.div`
@@ -19,19 +23,48 @@ const DayCell = tw.div`
   p-[1rem] text-s font-medium
 `;
 
+const ArrowContainer = tw.div`
+  flex justify-center items-center opacity-30 cursor-pointer
+  hover:opacity-100 transition-opacity duration-300 ease-in-out
+`;
+
 export default function Calendar() {
   const now = new Date();
-  const days = getDaysInMonth(now.getFullYear(), now.getMonth());
   const weekdays = ["일", "월", "화", "수", "목", "금", "토"];
+  const { viewDate, days, setDays, goToLeftMonth, goToRightMonth } =
+    useCalendarState();
+
+  useEffect(() => {
+    setDays(viewDate.getFullYear(), viewDate.getMonth() + 1);
+  }, [viewDate, setDays, goToLeftMonth, goToRightMonth]);
 
   return (
     <div className="bg-white rounded-lg w-full h-full p-[2rem]">
-      <h1 className="h-[4rem] text-l font-bold">{`${now.getFullYear()}.${(
-        now.getMonth() + 1
-      )
-        .toString()
-        .padStart(2, "0")}`}</h1>
-
+      <aside className="flex w-[15rem] items-center justify-between gap-[2rem]">
+        <h1 className="h-[4rem] text-l font-bold flex items-center justify-center">{`${viewDate.getFullYear()}.${(
+          viewDate.getMonth() + 1
+        )
+          .toString()
+          .padStart(2, "0")}`}</h1>
+        <div className="flex">
+          <ArrowContainer onClick={goToLeftMonth}>
+            <Image
+              src="/leftArrow.png"
+              alt="왼쪽 화살표"
+              width={16}
+              height={16}
+            />
+          </ArrowContainer>
+          <ArrowContainer onClick={goToRightMonth}>
+            <Image
+              src="/rightArrow.png"
+              alt="왼쪽 화살표"
+              width={16}
+              height={16}
+            />
+          </ArrowContainer>
+        </div>
+      </aside>
       <WeekContainer>
         {weekdays.map((week, index) => (
           <WeekCell key={index}>{week}</WeekCell>
--- src/store/calendarDay/index.tsx
+++ src/store/calendarDay/index.tsx
@@ -2,50 +2,47 @@ import { getDaysInMonth } from "@/utils/date";
 import { create } from "zustand";
 
 interface CalendarState {
-  now: Date;
+  viewDate: Date;
   days: (Date | null)[];
-  setDays: () => void;
+  setDays: (year: number, month: number) => void;
   goToLeftMonth: () => void;
   goToRightMonth: () => void;
 }
 
 // 캘린더에 나타나는 날짜 배열
 const useCalendarState = create<CalendarState>((set) => ({
-  now: new Date(),
+  viewDate: new Date(),
   days: [],
-  setDays: () =>
-    set((state) => {
-      const days = getDaysInMonth(
-        state.now.getFullYear(),
-        state.now.getMonth()
-      );
+  setDays: (year, month) =>
+    set(() => {
+      const days = getDaysInMonth(year, month);
       return { days };
     }),
   goToLeftMonth: () =>
     set((state) => {
       const prevMonth = new Date(
-        state.now.getFullYear(),
-        state.now.getMonth() - 1,
+        state.viewDate.getFullYear(),
+        state.viewDate.getMonth() - 1,
         1
       );
       const days = getDaysInMonth(
         prevMonth.getFullYear(),
         prevMonth.getMonth()
       );
-      return { now: prevMonth, days };
+      return { viewDate: prevMonth, days };
     }),
   goToRightMonth: () =>
     set((state) => {
       const nextMonth = new Date(
-        state.now.getFullYear(),
-        state.now.getMonth() + 1,
+        state.viewDate.getFullYear(),
+        state.viewDate.getMonth() + 1,
         1
       );
       const days = getDaysInMonth(
         nextMonth.getFullYear(),
         nextMonth.getMonth()
       );
-      return { now: nextMonth, days };
+      return { viewDate: nextMonth, days };
     }),
 }));
 
