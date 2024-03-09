7ce1a122c4a763061627c74faf199500492259d3
ejinn1 <ejinn1@naver.com>
2024-03-09 17:54:33 +0900

    style: tailwind custom transition 추가(height)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,6 +44,9 @@ const config: Config = {
         pulse: "pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       },
     },
+    transitionProperty: {
+      height: "height",
+    },
   },
   plugins: [],
 };
