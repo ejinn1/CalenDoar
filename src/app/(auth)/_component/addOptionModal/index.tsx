6df856cb4feeb7204e40f0c5dda538ac917d2921
ejinn1 <ejinn1@naver.com>
2024-03-09 17:55:16 +0900

    feat: nav 레이아웃 변경, nav메뉴 클릭시 nav길어짐

--- src/app/(auth)/_component/addOptionModal/index.tsx
+++ src/app/(auth)/_component/addOptionModal/index.tsx
@@ -4,7 +4,7 @@ interface Prop {
 
 export default function AddOptionModal({ onClose }: Prop) {
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-30 flex items-center justify-center">
+    <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-30 flex items-center justify-center z-10">
       <div className="bg-white p-[2rem] rounded-lg w-[40rem] h-[40rem] shadow-md">
         <h1>옵션 추가 모달</h1>
         <span onClick={onClose}>X</span>
