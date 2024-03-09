e011ae7ee6374fab91d3926fab4f89b559ad207a
ejinn1 <ejinn1@naver.com>
2024-03-09 15:18:37 +0900

    fix: nav 레이아웃 변경

--- src/app/_component/nav/index.tsx
+++ src/app/_component/nav/index.tsx
@@ -1,29 +1,34 @@
 import tw from "tailwind-styled-components";
 
-const StyledNav = tw.nav`
-    group
-    fixed top-0 left-0 h-[8rem] bg-lightgray w-full p-2 px-[8rem]
-    flex justify-between items-center
-    transition-bg duration-300 ease-in-out
-    hover:bg-gray
+const NavContainer = tw.nav`
+  group
+  fixed top-0 left-0 h-[8rem] w-full px-[2rem] pt-[2rem]
+  flex justify-center items-center
+`;
+
+const StyledNav = tw.div`
+  w-full h-full bg-lightgray rounded-lg px-[4rem]
+  flex justify-between items-center
+  transition-bg duration-300 ease-in-out
 `;
 
 const StyledUl = tw.ul`
-    flex gap-4 text-xs
-    group-hover:text-lightgray
+    flex gap-[1.5rem] text-m
 `;
 
 export default function Nav() {
   return (
-    <StyledNav>
-      <div className="text-xs font-semibold">Calendar</div>
-      <div>
-        <StyledUl>
-          <li>일정</li>
-          <li>할일</li>
-          <li>프로필</li>
-        </StyledUl>
-      </div>
-    </StyledNav>
+    <NavContainer>
+      <StyledNav>
+        <div className="text-l font-bold">Calendar</div>
+        <div>
+          <StyledUl>
+            <li>일정</li>
+            <li>할일</li>
+            <li>프로필</li>
+          </StyledUl>
+        </div>
+      </StyledNav>
+    </NavContainer>
   );
 }
