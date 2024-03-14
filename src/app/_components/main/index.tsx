8ae71e1945524b563587961540db80e0e0770697
ejinn1 <ejinn1@naver.com>
2024-03-14 16:45:22 +0900

    chore: middleware로 리다이렉트 실패, main에서 session이 없으면 그때 로그인페이지로 리다이렉트시킴

--- src/app/_components/main/index.tsx
+++ src/app/_components/main/index.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import DayContainer from "@/app/(auth)/_components/dayContainer";
+import { createClient } from "@/libs/supabase/client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import tw from "tailwind-styled-components";
 import Calendar from "../../(auth)/_components/calendar";
 
@@ -10,6 +13,22 @@ const StyledMain = tw.main`
 `;
 
 export default function Main() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const supabase = createClient();
+    const getSession = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      if (!session) {
+        router.push("/login");
+      }
+    };
+    getSession();
+  }, []);
+
   return (
     <StyledMain>
       <section className="w-full h-full">
