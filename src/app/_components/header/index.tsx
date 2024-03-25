7993f9b97b8b9622b3897da708cc4af3831983be
ejinn1 <ejinn1@naver.com>
2024-03-25 17:28:13 +0900

    style: haeder 스타일 변경(높이 조정)

--- src/app/_components/header/index.tsx
+++ src/app/_components/header/index.tsx
@@ -6,37 +6,37 @@ import tw from "tailwind-styled-components";
 
 const Container = tw.header`
   group
-  fixed top-0 left-0 w-full px-[2rem] pt-[2rem] z-10
-  flex justify-center items-center
-  transition-height duration-300 ease-in-out
+  fixed top-0 left-0 w-full px-[1rem] pt-[1rem] z-10 h-[8rem]
+  flex justify-center items-start
+
 `;
 
 const StyledNav = tw.div`
   relative
-  w-full h-full bg-lightgray rounded-lg px-[4rem] pt-[2rem]
-  flex justify-between items-start
+  w-full rounded-lg px-[2rem] bg-lightgray pt-[1.2rem]
+  flex justify-between
+  transition-height duration-300 ease-in-out
 `;
 
-// Option박스 섹션추가, 간격맞추기 해야됨
 export default function Header() {
   const [extended, setExtended] = useState(false);
 
   return (
-    <Container className={`${extended ? "h-[30rem]" : "h-[8rem]"}`}>
+    <Container>
       {extended && (
         <div className="fixed top-0 left-0 w-screen h-screen backdrop-blur-sm bg-black/20 "></div>
       )}
-      <StyledNav>
+      <StyledNav className={`${extended ? "h-[20rem]" : "h-[4rem]"}`}>
         <div className="text-l font-bold">CalenDoar</div>
-        <div className="flex gap-[2rem]">
+        <div className="flex">
           <div
             onClick={() => setExtended((prev) => !prev)}
             className={`cursor-pointer opacity-100 hover:opacity-50 transition-opacity duration-300 ease-in-out`}
           >
             {extended ? (
-              <Image src="/close.png" alt="닫기" width={20} height={20} />
+              <Image src="/close.png" alt="닫기" width={16} height={16} />
             ) : (
-              <Image src="/menu.png" alt="메뉴" width={20} height={20} />
+              <Image src="/menu.png" alt="메뉴" width={16} height={16} />
             )}
           </div>
         </div>
