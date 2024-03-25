805deb438693b33309550dd217f0a72b8084619d
ejinn1 <ejinn1@naver.com>
2024-03-25 17:34:19 +0900

    style: gap 간격 조정

--- src/app/_components/sideMenu/index.tsx
+++ src/app/_components/sideMenu/index.tsx
@@ -3,7 +3,7 @@ import Profile from "./_components/profile";
 
 export default function SideMenu() {
   return (
-    <section className="w-max h-full flex flex-col items-center gap-[2rem]">
+    <section className="w-max h-full flex flex-col items-center gap-[1rem]">
       <Profile />
       <OptionBox />
     </section>
