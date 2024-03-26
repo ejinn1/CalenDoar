223d7d2a386d37024f685df15f87941550f97401
ejinn1 <ejinn1@naver.com>
2024-03-26 16:57:00 +0900

    style: 기간설정 캘린더 글씨 더 잘보이게 수정

--- src/app/(auth)/_components/choseDateBox/index.tsx
+++ src/app/(auth)/_components/choseDateBox/index.tsx
@@ -164,7 +164,7 @@ export default function ChoseDateBox({ onClose, clickedDate }: Prop) {
                 ${day ? "hover:bg-lightgray  cursor-pointer" : ""}
                 ${
                   day && isAfter(day, startDate) && isBefore(day, endDate)
-                    ? "bg-lightblue opacity-15"
+                    ? "bg-lightblue opacity-40"
                     : ""
                 }
                 `}
