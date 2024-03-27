5a9e2123d02588e5ca823093f5a4f238cae313b2
ejinn1 <ejinn1@naver.com>
2024-03-27 18:08:40 +0900

    feat: 캘린더 weekRow 컴포넌트로 행마다 같은 높이 가지도록 수정

--- src/app/(auth)/_components/dayContainer/index.tsx
+++ src/app/(auth)/_components/dayContainer/index.tsx
@@ -2,13 +2,12 @@ import useModalOpen from "@/hooks/useModalOpen";
 import { createClient } from "@/libs/supabase/client";
 import useCalendarState from "@/store/calendarDay";
 import useOptionState from "@/store/options";
-import { isAfter, isSameDay, isSameOrBefore } from "@/utils/date";
 import { getOptionIdOfPath } from "@/utils/path";
 import { usePathname } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
 import tw from "tailwind-styled-components";
 import AddEventModal from "../addEventModal";
-import DayCell from "../dayCell";
+import WeekRow from "../weekRow";
 
 interface Event {
   title: string;
@@ -22,7 +21,7 @@ interface Event {
 
 const Container = tw.div`
   w-full h-[calc(100%-7rem)]
-  grid grid-cols-7 grid-auto-row
+  overflow-scroll
 `;
 
 export default function DayContainer() {
@@ -52,7 +51,7 @@ export default function DayContainer() {
   const { isOpen, openModal, closeModal } = useModalOpen();
   const [clickedDay, setClickedDay] = useState<Date>();
 
-  const [events, setEvents] = useState<any>();
+  const [events, setEvents] = useState<Event[]>();
 
   const path = usePathname();
   const { options } = useOptionState();
@@ -69,16 +68,16 @@ export default function DayContainer() {
 
     const getEvents = async () => {
       const {
-        data: { session },
-      } = await supabase.auth.getSession();
+        data: { user },
+      } = await supabase.auth.getUser();
 
-      if (!session) return;
+      if (!user) return;
 
       const { data, error } = await supabase
         .from("events")
         .select()
         .eq("option_id", optionId)
-        .eq("user_id", session.user.id);
+        .eq("user_id", user.id);
 
       if (error) {
         console.log("조회오류", error);
@@ -93,48 +92,48 @@ export default function DayContainer() {
 
   return (
     <Container>
-      {days.map((day, index) => (
-        <DayCell
-          key={index}
-          day={day}
-          setClickedDay={setClickedDay}
-          onOpen={openModal}
-          className={`${day !== null ? "shadow-sm cursor-pointer" : ""} ${
-            day?.getDay() === 0 ? "text-lightred" : ""
-          }`}
-        >
-          <span
-            className={`flex justify-center items-center rounded-full w-[2rem] h-[2rem] ${
-              toDayCheck && now.getDate() === day?.getDate()
-                ? "bg-lightred text-white"
-                : ""
-            }`}
-          >
-            {day ? day.getDate() : ""}
-          </span>
-          <span className="absolute top-[3.2rem] w-full left-0 flex flex-col">
-            {day &&
-              events &&
-              events
-                .filter((event: Event) => {
-                  const eventStart = new Date(event.start_date);
-                  const eventEnd = new Date(event.end_date);
-                  return (
-                    isSameDay(eventStart, day) ||
-                    (isAfter(day, eventStart) && isSameOrBefore(day, eventEnd))
-                  );
-                })
-                .map((event: Event, eventIndex: number) => (
-                  <span
-                    key={eventIndex}
-                    className="w-full bg-lightgray h-max text-r flex justify-center"
-                  >
-                    {event.title}
-                  </span>
-                ))}
-          </span>
-        </DayCell>
-      ))}
+      <WeekRow
+        days={days.slice(0, 7)}
+        events={events}
+        setClickedDay={() => setClickedDay}
+        onOpen={openModal}
+        toDayCheck={toDayCheck}
+      />
+      <WeekRow
+        days={days.slice(7, 14)}
+        events={events}
+        setClickedDay={() => setClickedDay}
+        onOpen={openModal}
+        toDayCheck={toDayCheck}
+      />
+      <WeekRow
+        days={days.slice(14, 21)}
+        events={events}
+        setClickedDay={() => setClickedDay}
+        onOpen={openModal}
+        toDayCheck={toDayCheck}
+      />
+      <WeekRow
+        days={days.slice(21, 28)}
+        events={events}
+        setClickedDay={() => setClickedDay}
+        onOpen={openModal}
+        toDayCheck={toDayCheck}
+      />
+      <WeekRow
+        days={days.slice(28, 35)}
+        events={events}
+        setClickedDay={() => setClickedDay}
+        onOpen={openModal}
+        toDayCheck={toDayCheck}
+      />
+      <WeekRow
+        days={days.slice(35, 42)}
+        events={events}
+        setClickedDay={() => setClickedDay}
+        onOpen={openModal}
+        toDayCheck={toDayCheck}
+      />
       {isOpen && clickedDay && (
         <AddEventModal day={clickedDay} onClose={closeModal} />
       )}
---
+++ src/app/(auth)/_components/weekRow/index.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { isAfter, isSameDay, isSameOrBefore } from "@/utils/date";
+import DayCell from "../dayCell";
+
+interface Props {
+  days: (Date | null)[];
+  events?: Event[];
+  setClickedDay: () => void;
+  onOpen: () => void;
+  toDayCheck: boolean;
+}
+
+interface Event {
+  title: string;
+  body: string;
+  start_date: string;
+  end_date: string;
+  start_time: string;
+  end_time: string;
+  check: boolean;
+}
+
+export default function WeekRow({
+  days,
+  events,
+  setClickedDay,
+  onOpen,
+  toDayCheck,
+}: Props) {
+  const now = new Date();
+
+  return (
+    <div className="grid grid-cols-7 h-max min-h-[10rem]">
+      {days.map((day, index) => (
+        <DayCell
+          key={index}
+          day={day}
+          setClickedDay={setClickedDay}
+          onOpen={onOpen}
+          className={`${day !== null ? "shadow-sm cursor-pointer" : ""} ${
+            day?.getDay() === 0 ? "text-lightred" : ""
+          }`}
+        >
+          <span
+            className={`flex justify-center items-center rounded-full w-[1.7rem] h-[1.7rem] ${
+              toDayCheck && now.getDate() === day?.getDate()
+                ? "bg-lightred text-white"
+                : ""
+            }`}
+          >
+            {day ? day.getDate() : ""}
+          </span>
+          <span className="w-full flex justify-center items-center flex-col gap-2 mt-2">
+            {day &&
+              events &&
+              events
+                .filter((event: Event) => {
+                  const eventStart = new Date(event.start_date);
+                  const eventEnd = new Date(event.end_date);
+                  return (
+                    isSameDay(eventStart, day) ||
+                    (isAfter(day, eventStart) && isSameOrBefore(day, eventEnd))
+                  );
+                })
+                .map((event: Event, eventIndex: number) => (
+                  <span
+                    key={eventIndex}
+                    className="w-full bg-lightgray h-max text-r rounded-sm flex justify-center"
+                  >
+                    {event.title}
+                  </span>
+                ))}
+          </span>
+        </DayCell>
+      ))}
+    </div>
+  );
+}
