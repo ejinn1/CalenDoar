45d491de5b06f27aeda978adc3388c7d6b13cf4a
ejinn1 <ejinn1@naver.com>
2024-03-30 12:50:49 +0900

    chore: 주말 일정 텍스트 검정색으로 변경

--- src/app/(auth)/_components/weekRow/index.tsx
+++ src/app/(auth)/_components/weekRow/index.tsx
@@ -2,6 +2,7 @@
 
 import { Event } from "@/store/events";
 import { isAfter, isSameDay, isSameOrBefore } from "@/utils/date";
+import tw from "tailwind-styled-components";
 import DayCell from "../dayCell";
 
 interface Props {
@@ -11,6 +12,14 @@ interface Props {
   onOpen: () => void;
   toDayCheck: boolean;
 }
+
+const WeekContainer = tw.div`
+  grid grid-cols-7 h-max min-h-[10.2rem]
+`;
+
+const EventsContainer = tw.span`
+  w-full flex justify-center items-center flex-col gap-2 mt-2
+`;
 
 export default function WeekRow({
   days,
@@ -22,7 +31,7 @@ export default function WeekRow({
   const now = new Date();
 
   return (
-    <div className="grid grid-cols-7 h-max min-h-[10.2rem]">
+    <WeekContainer>
       {days.map((day, index) => (
         <DayCell
           key={index}
@@ -42,14 +51,13 @@ export default function WeekRow({
           >
             {day ? day.getDate() : ""}
           </span>
-          <span className="w-full flex justify-center items-center flex-col gap-2 mt-2">
+          <EventsContainer>
             {day &&
               events &&
               events
                 .filter((event: Event) => {
                   const eventStart = new Date(event.start_date);
                   const eventEnd = new Date(event.end_date);
-
                   return (
                     isSameDay(eventStart, day) ||
                     (isAfter(day, eventStart) && isSameOrBefore(day, eventEnd))
@@ -58,14 +66,14 @@ export default function WeekRow({
                 .map((event: Event, eventIndex: number) => (
                   <span
                     key={eventIndex}
-                    className="w-full bg-lightgray h-max text-r rounded-sm flex justify-center"
+                    className="w-full bg-lightgray h-max text-r rounded-sm flex justify-center text-black"
                   >
                     {event.title}
                   </span>
                 ))}
-          </span>
+          </EventsContainer>
         </DayCell>
       ))}
-    </div>
+    </WeekContainer>
   );
 }
