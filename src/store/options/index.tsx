b505316bdbc2185ca6bfb528941e6b4c4f83c07a
ejinn1 <ejinn1@naver.com>
2024-04-02 12:26:07 +0900

    fix: todo, schedule 옵션 삭제

--- src/store/options/index.tsx
+++ src/store/options/index.tsx
@@ -22,18 +22,6 @@ const useOptionState = create<OptionsState>((set) => ({
       link: "/",
       id: "a7a9a629-fc06-4fc3-99bd-7ba881e4fb0f",
     },
-    {
-      name: "일정",
-      color: "#D3D3D3",
-      link: "/schedule",
-      id: "7a76de77-c79e-4223-bc9a-8ddf7e619de8",
-    },
-    {
-      name: "할일",
-      color: "#D3D3D3",
-      link: "/todo",
-      id: "e8ab09a4-602f-4dc4-9a36-a2e1f86750af",
-    },
   ],
   setOptions: (setOptions: Option[]) =>
     set((state) => {
