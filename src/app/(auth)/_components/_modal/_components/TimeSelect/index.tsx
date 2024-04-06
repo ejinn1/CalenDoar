73b7e39df45a68550dc291c8cbdd3bff3a251c50
ejinn1 <ejinn1@naver.com>
2024-04-06 16:37:09 +0900

    chore: 시간설정 다크모드 적용

--- src/app/(auth)/_components/_modal/_components/TimeSelect/index.tsx
+++ src/app/(auth)/_components/_modal/_components/TimeSelect/index.tsx
@@ -6,7 +6,7 @@ interface Prop {
 
 export default function TimeSelect({ type, onSelect, onClose }: Prop) {
   return (
-    <div className="absolute bg-white z-10 border-2 rounded-lg border-lightgray top-0 left-0 h-[10rem] p-2 overflow-scroll flex flex-col text-center">
+    <div className="absolute bg-white dark:bg-darkgray z-10 border-2 rounded-lg border-lightgray top-0 left-0 h-[10rem] p-2 overflow-scroll flex flex-col text-center">
       {type === "hour" ? (
         <>
           {Array.from({ length: 25 }).map((_, index) => (
@@ -17,7 +17,7 @@ export default function TimeSelect({ type, onSelect, onClose }: Prop) {
                 onSelect(index);
                 onClose();
               }}
-              className="cursor-pointer hover:bg-lightgray rounded-md "
+              className="cursor-pointer hover:bg-lightgray dark:hover:bg-gray rounded-md "
             >
               {index}
             </span>
@@ -33,7 +33,7 @@ export default function TimeSelect({ type, onSelect, onClose }: Prop) {
                 onSelect(index * 10);
                 onClose();
               }}
-              className="cursor-pointer hover:bg-lightgray rounded-md "
+              className="cursor-pointer hover:bg-lightgray dark:hover:bg-gray rounded-md "
             >
               {index === 0 ? index.toString().padStart(2, "0") : index * 10}
             </span>
